418af8ab8dbcc846f5eb68c3e27cb95aa29dd8e1
Ryan Bibby <ryanbibby@Ryans-MacBook-Air.local>
2024-06-02 19:05:28 +0100

    Extract eyes

--- face.py
+++ face.py
@@ -1,10 +1,11 @@
 
 import math
 from .eye import Eye
+from .ticker import Ticker
 
 class Face:
     def __init__(self):
-        self.left_eye = None
+        self.left_eye = Eye(-25, 30)
         self.right_eye = Eye(25, 30)
 
         self.left_ear = None
@@ -13,8 +14,10 @@ class Face:
         self.mouth = None
 
         self.heart = None
+    
 
-    def render(self, ctx):
+
+    def render(self, ctx, emotion):
         # Ears
         ctx.linear_gradient(-55, -20, -35, 0)
         ctx.add_stop(0, (100,100,100), 1)
@@ -38,5 +41,17 @@ class Face:
         ctx.curve_to(-16, -35, 16, -35, 45, -10)
         ctx.fill()
 
-        # Eyes
-        self.right_eye.render_open(ctx)
+
+
+        if emotion == "sad":
+            self.right_eye.render_tear(ctx)
+        else:
+            self.right_eye.render_open(ctx)
+
+        # if emotion == "flirty" and self.show and (not self.manual):
+
+
+        if emotion == "flirty" and Ticker.is_active_frame():
+            self.left_eye.render_winking(ctx)
+        else:
+            self.left_eye.render_open(ctx)
